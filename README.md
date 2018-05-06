cd19839b44210558f94628a8b322ddca7a842688
Pfeiler <djed.pfeiler@gmail.com>
2018-05-06 17:35:00 +0200

    update

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-"# GoCryptoData" 
+# GoCryptoData
 
-First steps in GoLang.
-Query crypto quotes from API.
+Check if random ETH wallets have a balance.
+
+The possibility that this program chooses a wallet with a balance is almost zero (64^36 possibilities).
+But when it does, it saves the wallet address, private key and the balance into a file balance.txt.
+
+To make this work you have to compile the ethereum-go client first.
+
+(https://github.com/ethereum/go-ethereum/wiki/Developers'-Guide)
+
+To do this you need to have a gcc in the classpath (e.g. tdm64-gcc-5.1.0-2.exe for windows).
---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import(
+	"log"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"context"
+	"github.com/ethereum/go-ethereum/common"
+	"math/big"
+	"encoding/hex"
+	"os"
+	"fmt"
+)
+
+// yeah, its a key, use it as you want or replace with yours
+const EthServiceUrl = "https://mainnet.infura.io/6pgDwimM6ABLKOxTxa2j"
+
+func main() {
+	log.Printf("Start checking addresses ...")
+	count := 0
+	for {
+		checkRandomAddress()
+		count = count + 1
+		if count == 100 {
+			log.Printf("Checked 100 addresses")
+			count = 0
+		}
+	}
+}
+
+func checkRandomAddress() {
+	address, key := getRandomAddressAndKey()
+	balance := checkBalance(address)
+	// log.Printf("address=%s key=%s balance=%v", address.Hex(), key, balance)
+	if balance.Sign() != 0 {
+		alertBalance(balance, address, key)
+	}
+}
+
+func getRandomAddressAndKey() (common.Address, string) {
+	key, _ := crypto.GenerateKey()
+	keyString := hex.EncodeToString(key.D.Bytes())
+	address := crypto.PubkeyToAddress(key.PublicKey)
+	return address, keyString
+}
+
+func checkBalance(address common.Address) *big.Int {
+	// create an IPC based RPC connection to a remote node
+	conn, err := ethclient.Dial(EthServiceUrl)
+	if err != nil {
+		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+		os.Exit(-1)
+	}
+	// instantiate the contract and display its name
+	// var ctx, _ = context.WithTimeout(context.Background(), 10000)
+	balance, err := conn.BalanceAt(context.TODO(), address, nil)
+	if err != nil {
+		log.Fatalf("Error while getting a balance: %v", err)
+		os.Exit(-1)
+	}
+
+	return balance
+}
+
+func alertBalance(balance *big.Int, address common.Address, key string) {
+	info := fmt.Sprintf("Found balance %s with address %s and key %s", balance, address.Hex(), key)
+	log.Printf("################################################")
+	log.Printf(info)
+	log.Printf("################################################")
+
+	f, _ := os.OpenFile("balances.txt", os.O_APPEND|os.O_WRONLY, 0755)
+	fmt.Fprintf(f, "%s\n", info)
+	defer f.Close()
+
+}
+
